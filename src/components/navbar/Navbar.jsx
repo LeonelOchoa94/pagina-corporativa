b3dccc76486c1c3a0352a8042d47c5f619f2ea5c
Leonel Ochoa <lochoa337@gmail.com>
2022-06-09 20:47:22 -0300

    Navegador terminado

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 
+import './styles.css';
 export const Navbar = () => {
-   return (
-      <>
-         <nav class='navbar navbar-expand-lg bg-light'>
-            <div class='container-fluid'>
-               <a class='navbar-brand' href='#'>
-                  Navbar
-               </a>
-               <button
-                  class='navbar-toggler'
-                  type='button'
-                  data-bs-toggle='collapse'
-                  data-bs-target='#navbarNavAltMarkup'
-                  aria-controls='navbarNavAltMarkup'
-                  aria-expanded='false'
-                  aria-label='Toggle navigation'
-               >
-                  <span class='navbar-toggler-icon'></span>
-               </button>
-               <div class='collapse navbar-collapse' id='navbarNavAltMarkup'>
-                  <div class='navbar-nav'>
-                     <a class='nav-link active' aria-current='page' href='#'>
-                        Home
-                     </a>
-                     <a class='nav-link' href='#'>
-                        Features
-                     </a>
-                     <a class='nav-link' href='#'>
-                        Pricing
-                     </a>
-                     <a class='nav-link disabled'>Disabled</a>
-                  </div>
-               </div>
-            </div>
-         </nav>
-      </>
-   );
+	return (
+		<>
+			<nav className='nav container d-flex justify-content-between'>
+				<div className='navegador'>
+					<ul>
+						<li>
+							<a href='#'>Home</a>
+						</li>
+						<li>
+							<a href='#conocimiento'>Conocimientos</a>
+						</li>
+						<li>
+							<a href='#experiencia'>Experiencia</a>
+						</li>
+						<li>
+							<a href='#'>Estudios</a>
+						</li>
+						<li>
+							<a href='#'>Proyectos</a>
+						</li>
+						<li>
+							<a href='#'>Contacto</a>
+						</li>
+					</ul>
+				</div>
+				<div className='buttons'>
+					<button className='btn btn-secondary' disabled>
+						Sign In
+					</button>
+					<button className='btn btn-outline-secondary' disabled>
+						Sign Up
+					</button>
+				</div>
+			</nav>
+		</>
+	);
 };
